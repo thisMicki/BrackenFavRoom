481ec2e1ebc31298705d16db5b50ea7ebfa0364d
Micki09 <hamacher.mikko@gmx.de>
2024-01-23 10:51:02 +0100

    it should work now, maybe

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,13 +1,39 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
 namespace LethalCompanyTemplate
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [HarmonyPatch(typeof(FlowermanAI))]
+    [HarmonyPatch("DoAIInterval")]
+    class FlowermanAIPatch
+    {
+        static void Postfix(FlowermanAI __instance)
+        {
+            Vector3 favRoomPos = GameObject.Find("SmallRoom2(Clone)").transform.position;
+            if (__instance.favoriteSpot == null || favRoomPos == __instance.favoriteSpot.transform.position)
+                return;
+            if (favRoomPos == null)
+            {
+                Debug.LogError("No Backroom room found!");
+                return;
+            }
+            Debug.Log("Changing the Brackens favorite spot");
+            __instance.favoriteSpot.transform.position = favRoomPos;
+            Debug.Log($"Changed Brackens favorite spot to X:{favRoomPos.x}, Y:{favRoomPos.y}, Z:{favRoomPos.z}");
+        }
+    }
+
+    [BepInPlugin("thismicki.plugin.BrackenFavRoom", "BrackenFavRoom", "0.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            // Plugin startup logic
+            var harmony = new Harmony("thismicki.plugin.BrackenFavRoom");
+            harmony.PatchAll();
+            Logger.LogInfo($"Patched Methods: {harmony.GetPatchedMethods()}");
+
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace LethalCompanyTemplate
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "BrackenFavRoom";
-        public const string PLUGIN_NAME = "BrackenFavRoom";
+        public const string PLUGIN_GUID = "thismicki.plugin.BrackenFavRoom";
+        public const string PLUGIN_NAME = "thismicki.plugin.BrackenFavRoom";
         public const string PLUGIN_VERSION = "0.1.0";
     }
 }
