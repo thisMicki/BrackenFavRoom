3be330973c30e93b9651cde8b0f0dc7d718dfd4b
Micki <1058064@bkrnet.de>
2024-01-26 15:26:27 +0100

    Changed patched function from FlowermanAI to EnemyAI to hopefully actually fix this plugin not doing anything rn

--- Plugin.cs
+++ Plugin.cs
@@ -1,56 +1,62 @@
-﻿using System.Reflection;
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
 using UnityEngine;
-using Unity.Netcode;
 
 namespace BrackenFavRoom
 {
-    [HarmonyPatch(typeof(FlowermanAI))]
-    [HarmonyPatch("DoAIInterval")]
-    class FlowermanAIPatch
+    [HarmonyPatch(typeof(EnemyAI))] // Patch something in the EnemyAI class
+    class EnemyAIPatch
     {
-        //static bool errorSend = false;
-        static GameObject SmallRoom;
-        static Vector3 favRoomPos = Vector3.zero;
+        static GameObject smallRoom;
 
-        // [HarmonyPatch("Start")]
-        // static void Postfix(){
-        //     SmallRoom = GameObject.Find("SmallRoom2(Clone)");
-        // }
-
-        static void Postfix(FlowermanAI __instance)
+        [HarmonyPatch("Start")] // Patch that function
+        [HarmonyPostfix] // After it ran
+        static void StartPatch() // Start just runs once and its better to find the Backrooms just once so I put it here
         {
-            GameObject SmallRoom = GameObject.Find("SmallRoom2(Clone)"); ;
-            Vector3 favRoomPos;
+            smallRoom = GameObject.Find("SmallRoom2(Clone)"); // The Backroom is called "SmallRoom2" in game and when spawned it gets the "(Clone)" attached so it needs to be added here as well
+        }
 
-            if (SmallRoom == null)
-            {
-                //if(!errorSend)
-                Debug.LogError("BrackenFavRoom: No Backroom room found!");
-                return;
-                
-            }
-            favRoomPos = SmallRoom.transform.position;
+        [HarmonyPatch("ChooseFarthestNodeFromPosition")] // Patch that function
+        [HarmonyPostfix] // After it ran
+        static void ChooseFarthestNodeFromPositionPatch(EnemyAI __instance, ref Transform __result) // __instance is acting the class "EnemyAI" and __result is the return value from the base function
+        {
+            if (__instance is not FlowermanAI) return; // If this script is not attached to a Bracken, it shouldn't change the output
 
-            if (__instance.favoriteSpot == null || favRoomPos == __instance.favoriteSpot.transform.position)
+            if (smallRoom == null) // If there is no Backrooms spawned in, there is no need to try and change the Brackens favorit spot to it
             {
+                Debug.LogWarning("BrackenFavRoom: No Backroom room found!"); // Also warn the console about that
                 return;
             }
-            __instance.favoriteSpot.SetPositionAndRotation(favRoomPos, Quaternion.Euler(Vector3.zero));
-            __instance.SetDestinationToPosition(favRoomPos, true);
 
-            Debug.Log($"BrackenFavRoom: Changed Brackens favorite spot to X:{favRoomPos.x}, Y:{favRoomPos.y}, Z:{favRoomPos.z}");
+            // GameObject[] nodesTempArray = (
+            //     from x in __instance.allAINodes
+            //     orderby Vector3.Distance(pos, x.transform.position) descending // For some reason this doesn't work so I'll just do it more inefficient :)
+            //     select x).ToArray<GameObject>();
+
+            GameObject closestNode = null; // If I don't assign null, I can't use it after the foreach loop
+            float closestDistance = 43210; // it's high just to make sure a node gets picked
+
+            foreach (GameObject node in __instance.allAINodes) // Check all nodes for the closeset one to the Backrooms
+            {
+                float distance = Vector3.Distance(smallRoom.transform.position, node.transform.position); // The distance between the Backrooms and the currently selected node
+                if (distance < closestDistance)
+                {
+                    closestNode = node;
+                    closestDistance = distance;
+                }
+            }
+            Debug.Log($"BrackenFavRoom: Changed Brackens favorite spot to X:{closestNode.transform.position.x}, Y:{closestNode.transform.position.y}, Z:{closestNode.transform.position.z}"); // say in the console that the poition has been changed
+            __result = closestNode.transform; // Change the return value of the base function
         }
     }
 
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    [BepInIncompatibility("readthisifbad-SnatchinBracken-1.2.8")]
+    [BepInIncompatibility("read__instanceifbad-SnatchinBracken-1.2.8")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            var harmony = new Harmony("thismicki.plugin.BrackenFavRoom");
+            var harmony = new Harmony("__instancemicki.plugin.BrackenFavRoom");
             harmony.PatchAll();
 
             // Plugin startup logic
