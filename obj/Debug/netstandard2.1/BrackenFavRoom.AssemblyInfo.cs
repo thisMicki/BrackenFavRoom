cf2137e2dca0384037934f6f95ad109f5818e25d
Micki <hamacher.mikko@gmx.de>
2024-03-05 18:45:39 +0100

    added game dependencies to repo

--- obj/Debug/netstandard2.1/BrackenFavRoom.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/BrackenFavRoom.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Change the brackes favorite spot to the Backrooms")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.9.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.9+20c809744fe9aef5765e6ddd43f312387406e5c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrackenFavRoom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrackenFavRoom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.9.0")]
